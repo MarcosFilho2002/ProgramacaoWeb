7705e5e4761f968e0e1902911435cf92ecf660f4
Marcos Bueno <mabf007@gmail.com>
2022-10-26 13:42:02 -0300

    Arrumando validações dos campos

--- javascript/script.js
+++ javascript/script.js
@@ -69,6 +69,69 @@ document.querySelector('.verso-carta8').addEventListener('click', async () =>{
     document.querySelector('.verso-carta8').src = json.data.card_images[0].image_url_small;
 });
 
+//Search
+document.querySelector(".cardName")
+.addEventListener("keyup", async () => {
+    let busca = this.value;
+    let cardname = document.querySelector(".cardName").value;
+    let cartas = await axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+    .then(function (response) {
+        console.log(response.data.data);
+        console.log(response.data.data[0].name);
+        let tamanhoArray = response.data.data.length;
+        const todasCartas = [];
+        for(let i = 0; i < tamanhoArray; i++){
+            todasCartas.push(response.data.data[i].name);
+        }
+        console.log(todasCartas);
+        todasCartas.filter((cardname) =>{
+            const valorMinusculo = valor.toLowerCase();  
+            const cidadeMinusculo = valor.toLowerCase();
+        })
+    })
+    .catch(function(error){
+        console.log('Error: '+error);
+    });
+    
+    /*console.log(cidades[0].innerHTML.search(busca));
+    for(let i = 0; i < cidades.length; i++){
+        if (cidades[i].innerHTML.search(busca) >= 0) {
+            cidades[i].style.display = "block";
+        }else{
+            cidades[i].style.display = "none";
+        }
+    }
+    */
+});
+/*
+function autoComplete(cidade) {
+    const destino = ['Salvador', 'Vitória', 'Maceió', 'Ceará','Rio Branco','Macapá', 'Porto Velho', 'Olinda','Aracaju','Capitólio','São Paulo', 'Paraty'];        
+    return destino.filter((valor) => {                
+        const valorMinusculo = valor.toLowerCase()                
+        const cidadeMinusculo = cidade.toLowerCase()                
+        return valorMinusculo.includes(cidadeMinusculo)          
+    })   
+}  
+const campo = document.querySelector('.campo')  
+const sugestoes = document.querySelector('.sugestoes')  
+campo.addEventListener('input', ({ target }) => {      
+    const dadosDoCampo = target.value      
+    if(dadosDoCampo.length) {         
+        const autoCompleteValores = autoComplete(dadosDoCampo)         
+        sugestoes.innerHTML = 
+        `${autoCompleteValores.map((value) => {     
+            return ( `<li>${value}</li>`)
+        })
+        .join('')}
+        `
+    }}
+);
+*/
+
+
+
+
+
 //https://reqres.in
 
 let token = localStorage.getItem('token');
@@ -102,7 +165,6 @@ async function login(email,senha){
             "password": senha
         })
         .then(function (response) {
-            alert('Login realizado com sucesso!');
             console.log(response.data.token);
             localStorage.setItem('token', response.data.token);
             let conteudoApi = document.querySelector('.conteudo-api');
@@ -114,22 +176,85 @@ async function login(email,senha){
 
         })
         .catch(function(error){
-            alert('Error: '+error.response.data.error);
-            document.querySelector('.input-email').value = '';
-            document.querySelector('.input-senha').value = '';
+            console.log('Error: '+error.response.data.error);
+            let erro = error.response.data.error;
+            console.log(erro);
+            if(erro == 'user not found'){
+                let alerta = document.querySelector('.alerta-bd');
+                alerta.style.display = 'block';
+            }
+            /*
+            if (erro === 'Missing password') {
+                let alerta = document.querySelector('.alerta-senha');
+                alerta.style.display = 'block';
+                console.log('entrei no if')
+                document.querySelector('.input-senha').value = '';
+            }
+            if (erro === 'Missing email or username') {
+                let alerta = document.querySelector('.alerta-email');
+                alerta.style.display = 'block';
+                console.log('entrei no if')
+                document.querySelector('.input-email').value = '';
+                document.querySelector('.input-senha').value = '';
+            }
+            */
+            
         });
 };
+
+document.querySelector('.input-email').addEventListener('keyup', (event)=>{
+    let alerta = document.querySelector('.alerta-tres');
+    alerta.style.display = 'none';
+});
+document.querySelector('.input-senha').addEventListener('keyup', (event)=>{
+    let alerta = document.querySelector('.alerta-tres');
+    alerta.style.display = 'none';
+});
+
+document.querySelector('.input-email').addEventListener('keyup', (event)=>{
+    let alerta = document.querySelector('.alerta-email');
+    alerta.style.display = 'none';
+});
+
+document.querySelector('.input-senha').addEventListener('keyup', (event)=>{
+    let alerta = document.querySelector('.alerta-senha');
+    alerta.style.display = 'none';
+});
+
+document.querySelector('.input-email').addEventListener('keyup', (event)=>{
+    let alerta = document.querySelector('.alerta-bd');
+    alerta.style.display = 'none';
+});
+document.querySelector('.input-senha').addEventListener('keyup', (event)=>{
+    let alerta = document.querySelector('.alerta-bd');
+    alerta.style.display = 'none';
+});
+
 
 document.querySelector('.btn-entrar').addEventListener('click', ()=>{
     let email = document.querySelector('.input-email').value;
     let senha = document.querySelector('.input-senha').value;
 
     console.log("Email: "+email.length+" Senha: "+senha.length);
-    login(email,senha);
-    if(email.length < 3 || senha.length < 3){
-        alert('Email e senha devem ter no mínimo 3 caracteres cada!');
+
+    if(email.length === 0){
+        let alerta = document.querySelector('.alerta-email');
+        alerta.style.display = 'block';
+        console.log('entrei no if')
         document.querySelector('.input-email').value = '';
         document.querySelector('.input-senha').value = '';
+
+    }else if(senha.length === 0){
+        let alerta = document.querySelector('.alerta-senha');
+        alerta.style.display = 'block';
+
+    }else if(email.length < 3 || senha.length < 3){
+        let alerta = document.querySelector('.alerta-tres');
+        alerta.style.display = 'block';
+        //document.querySelector('.input-email').value = '';
+        //document.querySelector('.input-senha').value = '';
+    }else{
+        login(email,senha);
     }
     
     let form = document.querySelector('.form-login');
